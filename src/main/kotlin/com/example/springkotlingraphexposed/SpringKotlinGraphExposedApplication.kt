4f3c18d82b91c85a9554945ca802fb8f85af4cd0
Joonas Harjukallio <harjukallio@gmail.com>
2019-05-29 16:36:29 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/springkotlingraphexposed/SpringKotlinGraphExposedApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springkotlingraphexposed
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinGraphExposedApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinGraphExposedApplication>(*args)
+}
